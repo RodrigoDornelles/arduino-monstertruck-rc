88172d545d61ee5358c43cc16726245437d34023
RodrigoDornelles <rodrigo.barbosa.dornelles@gmail.com>
2018-10-04 09:50:54 -0300

    reduced start time

--- libraries/rc-connect/rc-connect.cpp
+++ libraries/rc-connect/rc-connect.cpp
@@ -4,70 +4,71 @@
 
 #include <rc-connect.h>
 
+/*
+ * Configuração do WIFI
+ */
+void RCwifi::Config() {
+	RCwifi::Init();
+	RCwifi::AttempConection();
+
+	if(WiFi.status() != WL_CONNECTED)
+		RCwifi::Host();
+
+	RCwifi::ShowIP();
+}
+/*
+ * Começar configuração wifi
+ */
 void RCwifi::Init(){
 	Serial.print("Started");
 	delay(1000);
 	Serial.begin(115200);
 }
 /*
- * Começar configuração wifi
- */
-void RCwifi::Config() {
-	RCwifi::Init();
-	RCwifi::AttempConection();
-	RCwifi::Host();
-	RCwifi::ShowIP();
-}
-/*
- * Buscar por ponto de wifi
+ * Buscar por pontos de wifi
  */
 void RCwifi::AttempConection() {
 	WiFi.mode(WIFI_STA);
-	Serial.print("\nTrying to connect to a wifi network...\n\n");
+	Serial.print("\nTrying to connect to a wifi network...");
 
 	for(int i=0, j=MAX_ACESSPOINTS; i < j; i++)
-	{
-		char string[60];
-		sprintf(string, "\n[%2d/%2d] Connecting to wifi %s\n", i,j, APssid[i]);
-		Serial.print(string);
-
 		if(RCwifi::Connect(APssid[i],APpswd[i]))
 			break;
-	}
 }
 /*
  * Ancorar AcessPoint
  */
 void RCwifi::Host(){
-	if(WiFi.status() != WL_CONNECTED)
-	{
-		Serial.println("connection failed!");
-		WiFi.mode(WIFI_AP);
-		WiFi.softAP(HOSTssid, HOSTpassword);
-		Serial.println("**WIFI ACESSPOINT HOST STARTED**\n");
-		char string[16];
-		sprintf(string, "SSID: %s \n Password: %s",HOSTssid, HOSTpassword);
-		Serial.print(string);
-		Serial.println("*******************************\n");
-	}
+	WiFi.mode(WIFI_AP);
+	WiFi.softAP(HOSTssid, HOSTpassword);
+	Serial.println("**WIFI ACESSPOINT HOST STARTED**\n");
+	char string[60];
+	sprintf(string, "SSID: %s \n Password: %s",HOSTssid, HOSTpassword);
+	Serial.print(string);
+	Serial.println("*******************************\n");
+	WifiAPmode=true;
 }
 /*
  * Conectar a um Wifi
  */
 int RCwifi::Connect(char ssid[20], char password[20]){
 	WiFi.begin(ssid, password);
+	
+	char string[60];
+	sprintf(string, "\n[%2d/%2d] Connecting to wifi %s ", i,j, APssid[i]);
+	Serial.print(string);
 
 	unsigned long attempt = millis();
 
-	while (WiFi.status() != WL_CONNECTED && millis() <  (attempt + 5000) ){
-	  delay(800);
+	while (WiFi.status() != WL_CONNECTED && millis() <  (attempt + 800) ){
+	  delay(260);
 	  Serial.print(".");
 	}
 
 	if(WiFi.status() != WL_CONNECTED)
 	{
 		Serial.print("\n!!! SUCESS !!!");
-		return 1;
+		return !WifiAPmode=false;
 	}
 	return 0;
 }
