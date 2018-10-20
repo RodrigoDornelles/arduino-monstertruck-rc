6e2bf95522f1d9b7b25c4adfde1afddf1d899174
RodrigoDornelles <rodrigo.barbosa.dornelles@gmail.com>
2018-10-20 15:12:08 -0300

    update 0.3.2

--- libraries/rc-display/RCDISPLAY.h
+++ libraries/rc-display/RCDISPLAY.h
@@ -5,16 +5,47 @@
 
 class RCDISPLAY{
   public:
-  	//construtor | destructor
+  	/*
+  	 * construtor | destructor
+  	 */
   	RCDISPLAY();
   	~RCDISPLAY();
-  	//funcs
-  	void displayShow(char text[5], bool d_points = false);
+  	/*
+  	 * Display message
+  	 */
+  	void displayShow(const char text[5], bool d_points = false, uint8_t mode = 0);
+   	void sliderShow(const char text[56], int speed);
+   	void run(const char text[5]="rc ");
+   	void error();
+   	/*
+   	 * Display processing
+   	 */
+  	void process(); 	
+  	
   private:
-	//Funcs
-	void nextFrame();
+  	/*
+  	 *	Anim Display Modes	
+		@MODE_NONE		0
+		@MODE_RUNNING	1 
+ 		@MODE_SLIDER	2
+  	 */
+  	uint8_t	modeType;
+  	//anim: run
+  	unsigned long tickRunning;
+  	uint8_t	running;
+  	char runningText[5];
+  	//anim: slider
+  	unsigned long tickSlider;
+  	uint8_t slider;
+  	char sliderText[60];
+  	int sliderSpeed;
+  	/*
+  	 * Animation display 
+  	 */
+	bool nextFrame();
+	/*
+	 * Convert Char to 7Seg Leet
+	 */
 	uint8_t s(char letter);
 };
-
-
 #endif
